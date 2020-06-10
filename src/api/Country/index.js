a49481d14d12a7e826b539e5228a3304dd474cf8
Henrique <henrique.sousa.99@gmail.com>
2020-06-10 21:44:45 +0100

    countries example try - not final

---
+++ src/api/Country/index.js
@@ -0,0 +1,20 @@
+export default class Country
+{
+  static async countries(country)
+  {
+    const sUrl = `https://api.covid19api.com/${country.trim()}`;
+
+    const oHeaders = { 
+      Country: "Nome",
+      Slug: "Slug",
+      ISO2: "Abrev"
+    };
+
+    const oResponse = await fetch(sUrl,
+    {
+            headers: oHeaders,
+    });
+    const oResult = await oResponse.json();
+    return oResult;
+  }
+}
