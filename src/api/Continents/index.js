604842b55703f87fd6f6539808e57f56e1c6d231
Henrique <henrique.sousa.99@gmail.com>
2020-06-13 16:35:08 +0100

    added total per Continent. General totals.

---
+++ src/api/Continents/index.js
@@ -0,0 +1,25 @@
+export default class API
+{
+  /*
+  Totais por continente (todos). Sempre atualizado ao dia.
+  Mortes, Recuperados, Ativos, Criticos, Casos por Milhão, População
+  */
+  static async getYesterdayContinent()
+  {
+    const sUrl = `https://corona.lmao.ninja/v2/continents?yesterday=true&sort=`;
+
+    const oHeaders = { 
+      "yesterday": "true"
+    };
+
+    const oResponse = await fetch(sUrl,
+    {
+      method: "GET",
+      headers: oHeaders,
+    });
+
+    const oResult = await oResponse.json();
+    
+    return oResult;
+  }
+}
