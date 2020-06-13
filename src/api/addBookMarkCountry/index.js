0a435e686a88086d6c745ae19e7a3d0c5acb9f24
Henrique <henrique.sousa.99@gmail.com>
2020-06-13 19:59:49 +0100

    function to use the bookmark selection, query info

---
+++ src/api/addBookMarkCountry/index.js
@@ -0,0 +1,22 @@
+export default class API
+{
+  //receives the array of bookmarks
+  static async getBookMarkedCountries(...bookmarks)
+  {
+    //joins the array to ask for the info for the countries selected
+    const sUrl = `https://corona.lmao.ninja/v2/countries/${bookmarks.join(',')}`;
+
+    const oHeaders = { 
+    };
+
+    const oResponse = await fetch(sUrl,
+    {
+      method: "GET",
+      headers: oHeaders,
+    });
+
+    const oResult = await oResponse.json();
+
+    return oResult;
+  }
+}
